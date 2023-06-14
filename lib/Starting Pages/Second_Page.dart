a0bb0173846c85a7e8eeea40bb3b73e4ffbbfa0d
mirza <mirzaibtisam@gmail.com>
2023-06-14 07:05:59 -0700

    14/1 June changing

--- lib/Starting Pages/Second_Page.dart
+++ lib/Starting Pages/Second_Page.dart
@@ -9,118 +9,127 @@ class Second_Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
+        body: Stack(
+      children: [
+        Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
               children: [
                 SvgPicture.asset(
                   "assets/Group 2373.svg",
                   fit: BoxFit.cover,
                 ),
+                SizedBox(height: 25),
                 Padding(
-                  padding: const EdgeInsets.only(top: 35, right: 15),
+                  padding: const EdgeInsets.only(left: 20),
                   child: Align(
-                    alignment: Alignment.topRight,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(builder: (BuildContext context) {
-                          return Sign_Up();
-                        }));
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(32),
-                        ),
-                        height: 35,
-                        width: MediaQuery.of(context).size.width / 5,
-                        child: Center(
-                          child: Text(
-                            "Skip",
-                            style: TextStyle(fontSize: 15, color: Colors.black),
-                          ),
-                        ),
-                      ),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Allowing Every campaign to be canvassed",
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
+                ),
+                SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Timely",
+                      style: TextStyle(fontSize: 15, color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Efficient",
+                      style: TextStyle(fontSize: 15, color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Local",
+                      style: TextStyle(fontSize: 15, color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Transparent",
+                      style: TextStyle(fontSize: 15, color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.1,
+                  height: 50,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (BuildContext context) {
+                          return Log_In();
+                        }));
+                      },
+                      style: ElevatedButton.styleFrom(
+                          primary: Color(0xffED7D2B),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(32))),
+                      child: Text(
+                        "Get Started",
+                        style: TextStyle(fontSize: 17, color: Colors.white),
+                      )),
                 )
               ],
             ),
-            SizedBox(height: 25),
-            Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: Align(alignment: Alignment.topLeft,
-                child: Text(
-                  "Allowing Every campaign to be canvassed",
-                  style: TextStyle(fontSize: 15, color: Colors.black,fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: Align(alignment: Alignment.topLeft,
-                child: Text(
-                  "Timely",
-                  style: TextStyle(fontSize: 15, color: Colors.black),
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: Align(alignment: Alignment.topLeft,
-                child: Text(
-                  "Efficient",
-                  style: TextStyle(fontSize: 15, color: Colors.black),
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: Align(alignment: Alignment.topLeft,
-                child: Text(
-                  "Local",
-                  style: TextStyle(fontSize: 15, color: Colors.black),
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: Align(alignment: Alignment.topLeft,
-                child: Text(
-                  "Transparent",
-                  style: TextStyle(fontSize: 15, color: Colors.black),
-                ),
-              ),
-            ),
-            SizedBox(height: 30),
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 1.1,
-              height: 50,
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (BuildContext context) {
-                      return Log_In();
-                    }));
-                  },
-                  style: ElevatedButton.styleFrom(
-                      primary: Color(0xffED7D2B),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(32))),
-                  child: Text(
-                    "Get Started",
-                    style: TextStyle(fontSize: 17, color: Colors.white),
-                  )),
-            )
-          ],
+          ),
         ),
-      ),
-    );
+        Padding(
+          padding: const EdgeInsets.only(top: 35, right: 15),
+          child: Align(
+            alignment: Alignment.topRight,
+            child: InkWell(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (BuildContext context) {
+                  return Sign_Up();
+                }));
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(32),
+                ),
+                height: 35,
+                width: MediaQuery.of(context).size.width / 5,
+                child: Center(
+                  child: Text(
+                    "Skip",
+                    style: TextStyle(fontSize: 15, color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
